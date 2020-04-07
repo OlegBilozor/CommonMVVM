53b74342c78b4b0147a95982dfce4bd044a60f43
Oleg <OlegBilozor08@gmail.com>
2020-04-07 21:28:23 +0300

    Добавьте файлы проекта.

---
+++ CommonMVVM/Common/DelegateCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CommonMVVM.Common
+{
+    public class DelegateCommand : ICommand
+    {
+        public event Action RequestCommand;
+        private readonly Action<object> _action;
+        private readonly Predicate<object> _predicate;
+
+        public DelegateCommand(Action<object> action, Predicate<object> predicate)
+        {
+            _action = action;
+            _predicate = predicate;
+        }
+
+        public DelegateCommand(Action<object> action)
+        {
+            _action = action;
+            _predicate = p => true;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _predicate?.Invoke(parameter) ?? false;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action?.Invoke(parameter);
+            RequestCommand?.Invoke();
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
---
+++ CommonMVVM/ViewModel/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using CommonMVVM.Common;
+
+namespace CommonMVVM.ViewModel
+{
+    public class BaseViewModel : BasePropertyChangedNotifier
+    {
+        public virtual bool SetValue<T>(ref T field, T newValue)
+        {
+            if (Equals(field, newValue)) return false;
+
+            field = newValue;
+            return true;
+        }
+    }
+}
